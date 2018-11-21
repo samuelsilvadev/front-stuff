5009ad38635b506625cda77b3ba9585ddc593eae
Samuel Silva <samuelsilvawb@gmail.com>
2018-11-21 08:10:23 +0100

    chore(natours): Add config files
    - Added main files to start the project.
    - Configured some gulp tasks to run the project.

---
+++ sites/natours/README.md
@@ -0,0 +1,16 @@
+# Natours
+
+Simple website made using HTML, CSS, JS, applied main best practices, but use test.
+
+## How to install
+
+...
+
+## Contact
+
+- samuelsilvawb@gmail.com
+- [Twitter](https://twitter.com/samuelsilvadev)
+
+## License
+
+Samuel Silva &copy; MIT%
---
+++ sites/natours/gulpfile.js
@@ -0,0 +1,76 @@
+const autoPrefixBrowserList = [
+	'last 2 version',
+	'safari 5',
+	'ie 8',
+	'ie 9',
+	'opera 12.1',
+	'ios 6',
+	'android 4'
+];
+
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+const plumber = require('gulp-plumber');
+const sourceMaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+
+gulp.task('sass', () => {
+	return gulp
+		.src('src/scss/**/*.scss')
+		.pipe(sourceMaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourceMaps.write())
+		.pipe(
+			autoprefixer({
+				browsers: autoPrefixBrowserList
+			})
+		)
+		.pipe(gulp.dest('dist/styles'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('scripts', () => {
+	return gulp
+		.src('src/**/*.js')
+		.pipe(sourceMaps.init())
+		.pipe(
+			babel({
+				presets: ['@babel/env']
+			})
+		)
+		.pipe(concat('bundle.js'))
+		.pipe(sourceMaps.write('.'))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', () => {
+	return gulp.src('src/**/*.html').pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy-assets', () => {
+	return gulp.src('src/assets/*').pipe(gulp.dest('dist/assets'));
+});
+
+gulp.task('server', () => {
+	browserSync.init({
+		server: {
+			baseDir: './dist/'
+		}
+	});
+
+	gulp.watch('./src/*.html').on('change', () => {
+		gulp.series('html')();
+		browserSync.reload();
+	});
+	gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+	gulp.watch('./src/js/*.js', gulp.series('scripts'));
+	gulp.watch('./src/assets/*', gulp.series('copy-assets'));
+});
+
+gulp.task('default', gulp.series('html', 'sass', 'scripts', 'copy-assets'));
